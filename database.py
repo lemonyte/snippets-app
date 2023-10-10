997adcad54cbf099f066cdef120a42b51c658f9d
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-10-09 18:14:04 -0700

    Make db method args positional-only

--- database.py
+++ database.py
@@ -18,15 +18,15 @@ class SnippetExistsError(Exception):
 
 class SnippetDB(ABC):
     @abstractmethod
-    def get(self: Self, id: UUID) -> Snippet:
+    def get(self: Self, id: UUID, /) -> Snippet:
         pass
 
     @abstractmethod
-    def put(self: Self, snippet: Snippet) -> UUID:
+    def put(self: Self, snippet: Snippet, /) -> UUID:
         pass
 
     @abstractmethod
-    def delete(self: Self, id: UUID) -> None:
+    def delete(self: Self, id: UUID, /) -> None:
         pass
 
 
@@ -36,21 +36,21 @@ class FileDB(SnippetDB):
         if not self.path.exists():
             self.path.mkdir(parents=True, exist_ok=True)
 
-    def get(self: Self, id: UUID) -> Snippet:
+    def get(self: Self, id: UUID, /) -> Snippet:
         try:
             path = self.path / f"{id}.json"
             return Snippet.model_validate_json(path.read_text(encoding="utf-8"))
         except OSError as exc:
             raise SnippetNotFoundError from exc
 
-    def put(self: Self, snippet: Snippet) -> UUID:
+    def put(self: Self, snippet: Snippet, /) -> UUID:
         path = self.path / f"{snippet.id}.json"
         if path.exists():
             raise SnippetExistsError
         path.write_text(snippet.model_dump_json(), encoding="utf-8")
         return snippet.id
 
-    def delete(self: Self, id: UUID) -> None:
+    def delete(self: Self, id: UUID, /) -> None:
         path = self.path / f"{id}.json"
         if path.exists():
             path.unlink()
@@ -60,21 +60,21 @@ class DetaDB(SnippetDB):
     def __init__(self: Self, name: str) -> None:
         self._db = Base(name)
 
-    def get(self: Self, id: UUID) -> Snippet:
+    def get(self: Self, id: UUID, /) -> Snippet:
         try:
             snippet = next(iter(self._db.fetch({"id": str(id)}, limit=1).items))
         except StopIteration as exc:
             raise SnippetNotFoundError from exc
         return Snippet.model_validate(snippet)
 
-    def put(self: Self, snippet: Snippet) -> UUID:
+    def put(self: Self, snippet: Snippet, /) -> UUID:
         try:
             self._db.insert(snippet.model_dump(mode="json"), key=f"{snippet.creation_time}-{snippet.id}")
         except Exception as exc:  # noqa: BLE001
             raise SnippetExistsError from exc
         return snippet.id
 
-    def delete(self: Self, id: UUID) -> None:
+    def delete(self: Self, id: UUID, /) -> None:
         snippet = self.get(id)
         self._db.delete(f"{snippet.creation_time}-{snippet.id}")
 
